55b1885e3b97392be25faf66d39cb598f0bb2164
root <root@ip-172-31-91-64.ec2.internal>
2019-08-02 08:04:15 +0000

    First push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Jai Mata Di
         </p>
         <a
           className="App-link"
